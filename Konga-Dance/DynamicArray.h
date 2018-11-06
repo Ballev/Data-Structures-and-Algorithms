a389045a25037c7f8c11d27d4f26797af5917656
ABalev <sashobalev@abv.bg>
2018-11-07 00:12:55 +0200

    Create DynamicArray.h

---
+++ Konga-Dance/DynamicArray.h
@@ -0,0 +1,44 @@
+// Class DynamicArray is used as a container to store all the Konga queues.
+// The access to the queues will be faster with dynamic array.
+
+
+#ifndef __DYNAMICARRAY_HEADER__
+#define __DYNAMICARRAY_HEADER__
+
+#include "LinkedList.h"
+
+typedef int queue;
+
+class DynamicArray {
+
+public:
+
+public:
+	DynamicArray();
+	DynamicArray(const queue&);
+	DynamicArray(const DynamicArray&);
+	DynamicArray& operator=(const DynamicArray&);
+	~DynamicArray();
+
+public:
+	void add(const queue&);
+	void removeLast();
+	void insertAt(const queue&, size_t);
+	void removeAt(size_t);
+	const size_t getCurSize() const;
+	void appendLists(LinkedList&, const LinkedList&);
+	void print();
+
+private:
+	
+	void clean();
+	void resize();
+	void copyFrom(const DynamicArray &);
+
+private:
+
+	queue* arr;
+	size_t curSize;
+	size_t cap;
+
+};
